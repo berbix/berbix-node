1139694b555433f0b1e5306041922cefed22b40d
Andrew Li <andrewli0725@gmail.com>
2021-03-05 15:23:07 -0800

    remove deprecated elements

--- examples/transactions.js
+++ examples/transactions.js
@@ -5,7 +5,6 @@ var run = async function () {
   var client = new berbix.Client({
     apiSecret: process.env.BERBIX_DEMO_CLIENT_SECRET,
     apiHost: process.env.BERBIX_DEMO_API_HOST,
-    //environment: 'sandbox',
   });
 
   console.log("created client");
@@ -32,27 +31,6 @@ var run = async function () {
   }
 
   console.log(tokens);
-
-  try {
-    var continuation = await client.createContinuation(tokens);
-    console.log(continuation);
-  } catch (e) {
-    console.log(e);
-  }
-
-  var fetchedTokens = await client.exchangeCode(process.env.BERBIX_DEMO_CODE);
-
-  console.log(fetchedTokens);
-
-  var toRefresh = new berbix.Tokens.fromRefresh(fetchedTokens.refreshToken);
-
-  try {
-    var transaction = await client.fetchTransaction(toRefresh);
-  } catch (e) {
-    console.log(e);
-  }
-
-  console.log(transaction);
 
   try {
     console.log(await client.deleteTransaction(tokens));
